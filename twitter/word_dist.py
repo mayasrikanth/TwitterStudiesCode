5f07b056d27042f67ac971d7b05b443a958eec08
Maya Srikanth <mayassrikanth@gmail.com>
2020-04-04 19:25:24 -0700

    Updated word_dist.py

--- twitter/word_dist.py
+++ twitter/word_dist.py
@@ -95,31 +95,31 @@ def parse_expression(expr):
     return word
 
 def display_closestwords_tsnescatterplot(word, results):
-    
+
     # arr = np.empty((0,300), dtype='f')
     # word_labels = [word]
 
     # # get close words
     # close_words = model.similar_by_word(word)
-    
+
     # # add the vector for each of the closest words to the array
     # arr = np.append(arr, np.array([model[word]]), axis=0)
     # for wrd_score in close_words:
     #     wrd_vector = model[wrd_score[0]]
     #     word_labels.append(wrd_score[0])
     #     arr = np.append(arr, np.array([wrd_vector]), axis=0)
-        
+
     # find tsne coords for 2 dimensions
     word_labels = [word]
     for res in results:
         word_labels.append(res[0])
 
     arr = np.empty((1,50))
-    
+
     for res in results:
-        
+
         arr = np.concatenate((arr, np.reshape(res[2], (1, 50))), axis = 0)
-        
+
     tsne = TSNE(n_components=2, random_state=0)
     np.set_printoptions(suppress=True)
     Y = tsne.fit_transform(arr)
@@ -134,7 +134,12 @@ def display_closestwords_tsnescatterplot(word, results):
         plt.annotate(label, xy=(x, y), xytext=(0, 0), textcoords='offset points')
     plt.xlim(x_coords.min()+0.00005, x_coords.max()+0.00005)
     plt.ylim(y_coords.min()+0.00005, y_coords.max()+0.00005)
-    plt.show()
+    #plt.show()
+
+    file_name = word + "Closest.png"
+    plt.savefig(file_name)
+    return
+
 
 
 def find_word_clusters(labels_array, cluster_labels):
@@ -145,13 +150,13 @@ def find_word_clusters(labels_array, cluster_labels):
   return cluster_to_words
 
 
-def process(num_results):
-    inpt = input('> ')
-    if inpt == 'exit':
-        print("EXITING")
-        exit()
+def process(num_results, start_word):
+    #inpt = input('> ')
+    #if inpt == 'exit':
+    #    print("EXITING")
+    #    exit()
     # start_word = parse_expression(inpt)
-    start_word = "MeToo"
+
     # err, results = word_arithmetic(start_word=start_word,
     #                               minus_words=minus_words,
     #                               plus_words=plus_words,
@@ -159,10 +164,11 @@ def process(num_results):
     #                               id_to_word=id_to_word,
     #                               df=df,
     #                               num_results=num_results)
-    
+
     results = find_nearest(start_word, df[word_to_id[start_word]], id_to_word, df, num_results, method='cosine')
-    
-    display_closestwords_tsnescatterplot(start_word, results)
+
+
+
     print("CLOSEST WORDS: ")
     if results:
         print()
@@ -172,7 +178,11 @@ def process(num_results):
     else:
         print('{} not found in the dataset.'.format(err), file=sys.stderr)
 
+    display_closestwords_tsnescatterplot(start_word, results)
 
+# Start the process of discovering keywords via cosine distance
+def discover_keywords(start_word, num_words):
+    process(num_words, start_word)
 
 
 def parse_args():
@@ -197,20 +207,41 @@ def parse_args():
                         help='GloVe vector file path (default: data/glove)')
     return parser.parse_args()
 
-if __name__ == '__main__':
-    
-    args = parse_args()
-    vector_file = 'GloVe-1.2/results/vectors_metoo.txt'
 
-    if args.num_words > 400000:
-        print('--num_words must be equal to or less than 400,000. Exiting.')
-        exit(1)
-    
-    df, labels_array = utils.build_word_vector_matrix(vector_file, args.num_words)
+
+# takes input of the form python word_dist.py key_word
+if __name__ == '__main__':
+
+    keyword = input('Starting word: ')
+    num_words = input('Number of keywords: ')
+    #args = parse_args()
+    vector_file = 'GloVe-1.2/vectors.txt'
+
+    num_lines = 0
+    with open(vector_file, 'r') as f:
+        for line in f:
+            num_lines += 1
+    #if args.num_words > 400000:
+    #    print('--num_words must be equal to or less than 400,000. Exiting.')
+    #    exit(1)
+
+    df, labels_array = utils.build_word_vector_matrix(vector_file, num_lines)
     word_to_id, id_to_word = utils.get_label_dictionaries(labels_array)
+
+
     # Processing the num_output parameter to visualize 15 closest words FIRST
-    # process(args.num_output)
-    
+    #keyword = sys.argv[-1]
+    discover_keywords(keyword, int(num_words))
+    discover = True
+    while discover == True:
+        keyword = input('Starting word: ')
+        num_words = input('Number of keywords: ')
+        if keyword == "quit":
+            discovered = False
+            break
+        discover_keywords(keyword, int(num_words))
+        
+
     # k-means clustering with 100 clusters
     kmeans_model = KMeans(init='k-means++', n_clusters=100, n_init=10)
     kmeans_model.fit(df)
@@ -218,8 +249,8 @@ if __name__ == '__main__':
     cluster_labels  = kmeans_model.labels_
     cluster_inertia   = kmeans_model.inertia_
     cluster_to_words  = find_word_clusters(labels_array, cluster_labels)
-    
-    
+
+
     count = 0
     arr = np.empty((1,50))
     word_labels = []
@@ -227,7 +258,7 @@ if __name__ == '__main__':
     for c in cluster_to_words:
         print(cluster_to_words[c])
         print("\n")
-        if "Epstein" in cluster_to_words[c] or "Kavannaugh" in cluster_to_words[c]:
+        if "hillary" in cluster_to_words[c] or "trump" in cluster_to_words[c]:
             j = 0
             for word in cluster_to_words[c]:
                 if word.lower() not in stopword and len(word) > 4:
@@ -248,24 +279,30 @@ if __name__ == '__main__':
     # print(x_coords)
     y_coords = Y[:, 1]
 
-      
+
     print(num)
     print(np.shape(x_coords))
     # Visualizing Kavannaugh and Epstein clusters (i.e. the 15 words closest to them)
     plt.scatter(x_coords[0:15], y_coords[0:15], c = "r")
     plt.scatter(x_coords[num[0]:num[0]+15], y_coords[num[0]:num[0]+15], c = "b")
 
+    group_1 = []
+    group_2 = []
+
     for label, x, y in zip(word_labels[0:15], x_coords[0:15], y_coords[0:15]):
+        group_1.append(label)
         plt.annotate(label, xy=(x, y), xytext=(0, 0), textcoords='offset points', fontsize=12, rotation=-20)
-    
+
     for label, x, y in zip(word_labels[num[0]:num[0]+15], x_coords[num[0]:num[0]+15], y_coords[num[0]:num[0]+15]):
+        group_2.append(label)
         plt.annotate(label, xy=(x, y), xytext=(0, 0), textcoords='offset points',fontsize=12, rotation=-10)
-    
+
     plt.xlim(x_coords.min()+0.00005, x_coords.max()+0.00005)
     plt.ylim(y_coords.min()+0.00005, y_coords.max()+0.00005)
     #plt.axis('off')
-  
-    plt.show()
-    process(args.num_output)
+    print("Subtopic 1: " + str(group_1))
+    print("Subtopic 2: " + str(group_2))
+    #plt.show()
+    #process(args.num_output)
     #while True:
          #process(args.num_output)
